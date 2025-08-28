d7e6a9fccf0ce0f2a495fc27c66eee4de56177c7
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2025-08-28 15:54:53 -0700

    add backspace and decimal button
    Backspace:
    - Lets the user undo the last input
    
    Decimal:
    - Lets the user input decimals
    
    Miscellaneous:
    - Scaled up the calculator

--- script.js
+++ script.js
@@ -11,35 +11,35 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return Math.round((a / b) * 10000) / 10000;
+    return a / b;
 }
 
-let firstNum, 
-    operator, 
+let firstNum,
+    operator,
     secondNum,
     result;
 
 const operate = function (firstNum, operator, secondNum) {
     switch (operator) {
-        case 'add': 
+        case 'add':
             result = add(firstNum, secondNum);
             break;
-        case 'subtract': 
+        case 'subtract':
             result = subtract(firstNum, secondNum);
             break;
-        case 'multiply': 
+        case 'multiply':
             result = multiply(firstNum, secondNum);
             break;
-        case 'divide': 
+        case 'divide':
             result = divide(firstNum, secondNum);
             break;
-        default: 
+        default:
             'error'
             break;
-        }
-    return Math.round(result * 10000) / 10000;
-
     }
+    return Math.round(result * 100000000) / 100000000;
+
+}
 
 const display = document.querySelector(".display");
 let displayValue = '';
@@ -48,39 +48,45 @@ digits = [...digits];
 let classes = [];
 
 for (let i = 0; i < digits.length; i++) {
-    
+
     classes.push(digits[i].getAttribute("class"));
-    
+
     digits[i].addEventListener("click", (event) => {
-    
+
         // Calculates value when inputting 2nd operator
-        if ((classes[i] === 'add' || classes[i] === 'subtract' || classes[i] === 'multiply' || classes[i] === 'divide' ) 
+        if ((classes[i] === 'add' || classes[i] === 'subtract' || classes[i] === 'multiply' || classes[i] === 'divide')
             && typeof firstNum === 'number') {
-        if (!displayValue) {
-        } else {
-            secondNum = Number(display.value);
-            displayValue = operate(firstNum, operator, secondNum);
-            display.value = displayValue;
+            if (!displayValue) {
+            } else {
+                secondNum = Number(display.value);
+                displayValue = operate(firstNum, operator, secondNum);
+                display.value = displayValue;
+                firstNum = Number(displayValue);
+                operator = classes[i];
+                displayValue = '';
+            }
+        }
+
+        
+        if (classes[i] === 'add' || classes[i] === 'subtract' || classes[i] === 'multiply' || classes[i] === 'divide') {
+            displayValue = display.value;
             firstNum = Number(displayValue);
             operator = classes[i];
             displayValue = '';
         }
-        }
-
-        if (classes[i] === 'add' || classes[i] === 'subtract' || classes[i] === 'multiply' || classes[i] === 'divide') {
-            displayValue = display.value; 
-            firstNum = Number(displayValue); 
-            operator = classes[i]; 
-            displayValue = ''; 
-        } 
         else if (classes[i] === 'clear') {
-            displayValue = '0'; 
+            displayValue = '0';
             firstNum = '';
             secondNum = '';
             operator = '';
+            document.querySelector('.decimal').toggleAttribute("disabled", false);
             display.value = displayValue;
-        } 
-        else if (classes[i] === "equal") {
+        }
+        else if (classes[i] === 'backspace') {
+            displayValue = display.value.slice(0, -1);
+            display.value = displayValue;
+        }
+        else if (classes[i] === 'equal') {
             secondNum = Number(displayValue);
             displayValue = operate(firstNum, operator, secondNum);
             display.value = displayValue;
@@ -88,9 +94,15 @@ for (let i = 0; i < digits.length; i++) {
             firstNum = 0;
         }
         else {
-            if (display.value === '0') {
+            if (displayValue.split(".").length - 1 === 1) {
+                document.querySelector('.decimal').toggleAttribute("disabled", true);
+            } else {
+                document.querySelector('.decimal').toggleAttribute("disabled", false);
+            }
+            if (display.value === '0' && classes[i] !== 'decimal') {
                 displayValue = '';
-            } 
+            } else {
+            }
             displayValue += event.target.textContent;
             display.value = displayValue;
         }
